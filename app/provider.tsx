6aa63cdbd50f857f20cc5b1f1f12ae6adc7acdc1
Vinodkumaarr <mastervinod2004@gmail.com>
2026-05-22 18:01:36 +0530

    Project setup:authentication and database

---
+++ app/provider.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { UserDetailContext } from '@/context/UserDetailContext';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    const [userDetail,setUserDetail] = useState<any>(null);
+
+     useEffect(()=>{
+        CreateNewUser();
+     },[])
+
+     const CreateNewUser = async()=>{
+        const result = await axios.post('/api/users',{});
+        
+        console.log("Result",result);
+        setUserDetail(result.data?.user);
+     }
+
+  return (
+    <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
+        <div>{children}</div>
+    </UserDetailContext.Provider>
+  )
+}
+
+export default Provider
